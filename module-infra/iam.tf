c4c917e054513b0660f5b97b2d365ed5139d549b
Meherun Mithila <mnm19322@email.vccs.edu>
2025-07-27 01:39:38 -0400

    change 16

--- module-infra/iam.tf
+++ module-infra/iam.tf
@@ -2,7 +2,7 @@
 resource "aws_iam_role" "main" {
   name = "${var.name}-role"
 
-  assume_role_policy = jsondecode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
